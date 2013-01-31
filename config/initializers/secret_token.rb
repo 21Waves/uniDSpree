fa930f1a67cd70555d4ec2d93a3966d0be42d2d1
Nelson Kelem <nelson@21waves.com>
2013-01-31 12:52:29 +0000

    inial ci

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UniDSpree::Application.config.secret_token = '5ab10c239365d00cb2f872613b8f05fe8a926e9615083e07237b44155f4a551db528d1f7ed6cd83e8929769da193362496e042eb312cb09fc5a8771415656a44'
